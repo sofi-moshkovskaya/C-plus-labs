dbc498fd1e30d4adc3744cc90dcc0239bfc464e7
sofi-moshkovskaya <49905202+sofi-moshkovskaya@users.noreply.github.com>
2019-04-24 22:49:15 +0300

    Add files via upload

---
+++ lab_1/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 5;
+	int y = 7;
+	cout << "\n";
+	cout << x + y << "   " << x * y;
+	cout << "\n";
+	cin.get();
+	return 0;
+}
---
+++ lab_1/Source2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello word\n";
+	cin.get();
+	return 0;
+}
